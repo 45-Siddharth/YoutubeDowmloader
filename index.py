5b9bc1c64393718078ac9ad1a8bb5b740d2757be
45-Siddharth <143333331+45-Siddharth@users.noreply.github.com>
2024-08-25 22:06:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,31 +1,71 @@
 from fastapi import FastAPI, HTTPException, Form
 import yt_dlp
+import os
+import tempfile
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.post("/download")
 def download_videos_from_channel(link: str = Form(...), num_videos: int = Form(...)):
-    ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4a]/best',
-        'outtmpl': '/storage/emulated/0/Download/%(title)s.%(ext)s',
-        'noplaylist': True,
-        'playlistend': num_videos,
-        'quiet': False,
-    }
     try:
+        temp_dir = tempfile.gettempdir()
+
+        output_template = os.path.join(temp_dir, '/storage/emulated/0/Download/%(title)s.%(ext)s')
+
+        ydl_opts = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4a]/best',
+            'outtmpl': output_template,
+            'noplaylist': True,
+            'playlistend': num_videos,
+            'quiet': False,
+        }
+
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([link])
-        return {"status": "Download started"}
+
+        return {"status": "Download started", "output_dir": temp_dir}
+
     except Exception as e:
         print(f"An error occurred: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+# from fastapi import FastAPI, HTTPException, Form
+# import yt_dlp
+# from fastapi.middleware.cors import CORSMiddleware
+
+# app = FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins = ["*"],
+#     allow_credentials = True,
+#     allow_methods = ["*"],
+#     allow_headers = ["*"],
+# )
+
+# @app.post("/download")
+# def download_videos_from_channel(link: str = Form(...), num_videos: int = Form(...)):
+#     ydl_opts = {
+#         'format': 'bestvideo[ext=mp4]+bestaudio[ext=mp4a]/best',
+#         'outtmpl': '/storage/emulated/0/Download/%(title)s.%(ext)s',
+#         'noplaylist': True,
+#         'playlistend': num_videos,
+#         'quiet': False,
+#     }
+#     try:
+#         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#             ydl.download([link])
+#         return {"status": "Download started"}
+#     except Exception as e:
+#         print(f"An error occurred: {e}")
+#         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
